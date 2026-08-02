8d60a68a81f08ed25105c55c03e11d0019d69d7c
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2026-08-02 09:16:44 +0100

    electron

---
+++ apps/phaser-game-desktop/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'phaser-game'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'phaser-game-desktop';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
